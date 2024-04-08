c03ca30a9882ea2fbe9388c4ca14dc131e8a4327
jared <114547220+goldjared@users.noreply.github.com>
2024-04-08 14:12:16 -0400

    format

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import express, { Express, Request, Response } from "express";
-import morgan from "morgan";
-import cors from "cors";
+import express, { Express, Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app: Express = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 morgan.token('body', (req: any, res) => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 interface Person {
   id: number;
@@ -18,45 +20,51 @@ interface Person {
 }
 
 let people: Person[] = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
 ];
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("PB Server 001");
+app.get('/', (req: Request, res: Response) => {
+  res.send('PB Server 001');
 });
 
-app.get("/people", (req: Request, res: Response) => {
-    res.json(people);
+app.get('/people', (req: Request, res: Response) => {
+  res.json(people);
 });
 
-app.get("/info", (req: Request, res: Response) => {
+app.get('/info', (req: Request, res: Response) => {
   const date: Date = new Date();
-   res.send("Phonebook has info for " + people.length + " people" + "<br/>" + date.toUTCString());
+  res.send(
+    'Phonebook has info for ' +
+      people.length +
+      ' people' +
+      '<br/>' +
+      date.toUTCString()
+  );
 });
 
 // app.get("/api/notes/:id", (req: Request, res: Response) => {
 //   const id: number = Number(req.params.id);
 //   const note = notes.find(note => note.id === id);
-//   if(note) {    
+//   if(note) {
 //     res.json(note);
 //   } else {
 //     res.status(404).end();
@@ -64,40 +72,40 @@ app.get("/info", (req: Request, res: Response) => {
 // });
 
 app.delete('/people/:id', (request, response) => {
-  console.log(people.length)
-  const id: number = Number(request.params.id)
-  people = people.filter(person => person.id !== id)
-  console.log(people.length)
+  console.log(people.length);
+  const id: number = Number(request.params.id);
+  people = people.filter((person) => person.id !== id);
+  console.log(people.length);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
 const randomVal = (max: number): number => {
   return Math.floor(Math.random() * max);
-}
+};
 
-app.post("/people", (req: Request, res: Response) => {
-  const person = req.body; 
-  if (!person.name || !person.id ) {
-    return res.status(400).json({ 
-      error: 'content missing' 
-    })
+app.post('/people', (req: Request, res: Response) => {
+  const person = req.body;
+  if (!person.name || !person.id) {
+    return res.status(400).json({
+      error: 'content missing',
+    });
   }
-
+  const test = '';
   const name = person.name;
   const id = person.id;
-  const serialId: string = randomVal(100) + "-" + randomVal(100) + "-" + randomVal(1000000);
+  const serialId: string =
+    randomVal(100) + '-' + randomVal(100) + '-' + randomVal(1000000);
 
   const newPerson = {
     id,
     name,
-    number: serialId
-  }
-  people.push(newPerson);  
+    number: serialId,
+  };
+  people.push(newPerson);
   res.json(newPerson);
 });
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-
