ecb8a8b9fae1345aec8f6bfad8a4809c295c81cc
jared <114547220+goldjared@users.noreply.github.com>
2024-05-21 21:07:27 -0400

    refactor routing methods to use mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
+dotenv.config();
+const Person = require('./models/persons');
 const app: Express = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -14,71 +15,48 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-interface Person {
-  id: number;
-  name: string;
-  number: string;
-}
-
-let people: Person[] = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('PB Server 001');
-});
-
 app.get('/people', (req: Request, res: Response) => {
-  res.json(people);
+  Person.find({}).then((result: any) => {
+    console.log(result.length);
+    res.json(result);
+  });
 });
 
 app.get('/info', (req: Request, res: Response) => {
   const date: Date = new Date();
-  res.send(
-    'Phonebook has info for ' +
-      people.length +
-      ' people' +
-      '<br/>' +
-      date.toUTCString()
-  );
+
+  Person.countDocuments({}).then((result: any) => {
+    res.send(
+      'Phonebook has info for ' +
+        result +
+        ' people' +
+        '<br/>' +
+        date.toUTCString()
+    );
+  });
 });
 
-// app.get("/api/notes/:id", (req: Request, res: Response) => {
-//   const id: number = Number(req.params.id);
-//   const note = notes.find(note => note.id === id);
-//   if(note) {
-//     res.json(note);
-//   } else {
-//     res.status(404).end();
-//   }
-// });
+app.get('/people/:id', (req: Request, res: Response, next: NextFunction) => {
+  Person.findById(req.params.id)
+    .then((person: any) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error: any) => {
+      console.log(error);
+      res.status(400).send({ error: 'malformatted id' });
+    });
+});
 
-app.delete('/people/:id', (request, response) => {
-  console.log(people.length);
-  const id: number = Number(request.params.id);
-  people = people.filter((person) => person.id !== id);
-  console.log(people.length);
-
-  response.status(204).end();
+app.delete('/people/:id', (req: Request, res: Response, next: NextFunction) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result: any) => {
+      res.status(204).end();
+    })
+    .catch((error: any) => next(error));
 });
 
 app.post('/people', (req: Request, res: Response) => {
@@ -88,19 +66,18 @@ app.post('/people', (req: Request, res: Response) => {
       error: 'content missing',
     });
   }
-  const name = person.name;
-  // id is incr by 1 of last id val, or if list empty id is 1
-  const id = people.length > 0 ? people[people.length - 1].id + 1 : 1;
-  const number: string = person.number;
-  const newPerson = {
-    id,
-    name,
-    number,
-  };
-  people.push(newPerson);
-  res.json(newPerson);
+
+  const newPerson = new Person({
+    name: person.name,
+    number: person.number,
+  });
+
+  newPerson.save().then((savedPerson: any) => {
+    res.json(savedPerson);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
